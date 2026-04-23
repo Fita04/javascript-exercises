40bf0a628fc229d70a91ae69886184a3428a6de7
Fita04 <crackers0404@gmail.com>
2026-04-23 19:21:04 +0800

    Completed Exercise 16 Completed exercise 16. I used a forEach loop with an if-else statement inside to filter the objects and apply the appropriate logic to get their age. Simply used .sort() to sort the objects based on their age in descending order Returns the first index of the modified array, which should be the oldest person

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,36 @@
-const findTheOldest = function() {
+const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
 
+const findTheOldest = function(arr) {
+    let currentYear = new Date().getFullYear();
+    arr.forEach((person) => 
+    {if (Object.hasOwn(person, "yearOfBirth") && Object.hasOwn(person, "yearOfDeath")) 
+        { person.age = person.yearOfDeath - person.yearOfBirth;
+        
+    } else { person.age = currentYear - person.yearOfBirth;
+        
+    }});
+    arr.sort((a, b) => b.age - a.age);
+    return arr[0]
 };
+
+findTheOldest(people);
+console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
